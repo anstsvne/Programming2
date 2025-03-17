2285106a158b8b2c43a9b041e30c95f2969a2ee6
anstsvne <ansts08137954@gmail.com>
2025-03-18 00:07:43 +0700

    Создание класса Contact

---
+++ src/Contacts/View/Model/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View.Model
+{
+    public class Contact
+    {
+        public string Name { get; set; } = "Tim";
+        public string PhoneNumber { get; set; } = "example@inbox.ru";
+        public string Email { get; set; } = "8-923-633-2689";
+    }
+}
