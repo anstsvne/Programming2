c9f4da9f632adada6a3208c7ac5cd3ee5b9a398b
anstsvne <ansts08137954@gmail.com>
2025-03-18 18:24:24 +0700

    Привязка данных в главном окне через DataContext

--- src/Contacts/View/MainWindow.xaml.cs
+++ src/Contacts/View/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using View.ViewModel;
 
 
 namespace View
@@ -24,6 +25,7 @@ namespace View
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainVM();
         }
     }
 }
---
+++ src/Contacts/View/ViewModel/SaveCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+using View.Model.Services;
+using View.Model;
+
+namespace View.ViewModel
+{
+    public class SaveCommand : ICommand
+    {
+        private readonly MainVM _viewModel;
+
+        public SaveCommand(MainVM viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter)
+        {
+            ContactSerializer.Save(new Contact
+            {
+                Name = _viewModel.Name,
+                PhoneNumber = _viewModel.PhoneNumber,
+                Email = _viewModel.Email
+            });
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
